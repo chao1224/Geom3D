17a58a3911fe7380aeef8b646905696684bbd35d
chao1224 <shengchao.hacker@gmail.com>
2023-06-07 15:21:27 -0400

    Add setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='Geom3D',
+      description='Geometric Modeling on 3D Data',
+      author='Shengchao Liu',
+      author_email='liusheng@mila.quebec',
+      license='MIT',
+      packages=find_packages())
